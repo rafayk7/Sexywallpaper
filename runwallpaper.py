4f2fee5819f01ac82f7ade343ca104ccf80ce867
Rafay Kalim <rafayk7@yahoo.com>
2019-07-08 17:02:49 -0400

    fix extraction

--- runwallpaper.py
+++ runwallpaper.py
@@ -1,47 +1,42 @@
-import os
-from PIL import Image
-from appscript import *
-import argparse
-import time
-import datetime
+from PIL import Image, ImageSequence
+from datetime import datetime
+
+class GenFrames:
+    def __init__(self, outFolder,gifPath):
+        self.frames = 0
+        self.outFolder = outFolder
+        self.gifPath = gifPath
+        self.timeBetweenChange = 0  # Minutes
+        self.SECONDSINDAY = 86400
+        self.times = []
+        self.currentImg = ''
+
+    def extractFrames(self):
+        gifPIL = Image.open(self.gifPath)
+
+        self.frames = gifPIL.n_frames
+        index = 1
+        for frame in ImageSequence.Iterator(gifPIL):
+            frame.save("%s/frame%d.png" % (self.outFolder, index))
+            index += 1
+
+        self.timeBetweenChange = self.SECONDSINDAY / self.frames
+
+    def getTimes(self):
+        #Need to run it every 6.67 minutes. Get current time, current minute of the day. modulu by self.timeBetweenChange. Change it to that image. Get remainder. Schedule it for now+that minutes
+        now = datetime.now()
+        midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        secondsElapsedInDay = (now - midnight).seconds
+        imgNumber = secondsElapsedInDay % self.timeBetweenChange
+
+        self.currentImg = "%s/frame%d.png" % (self.outFolder, imgNumber)
+        print(imgNumber)
+        print(self.currentImg)
 
 
-def extractFrames(gif, outFolder):
-    frame = Image.open(gif)
-    nframes = 0
-    while frame:
-        frame.save( '%s/%s-%s.gif' % (outFolder, os.path.basename(gif), nframes ) , 'GIF')
-        nframes += 1
-        try:
-            frame.seek( nframes )
-        except EOFError:
-            break;
-    return True
-    
-def setWallpaper(image):
-
-    se = app('System Events')
-    desktops = se.desktops.display_name.get()
-
-    for d in desktops:
-        desk = se.desktops[its.display_name == d]
-        desk.picture.set(mactypes.File(image))
 
 
-
-#extractFrames('good.gif', 'frames')
-
-t1 = datetime.datetime.now()
-t2 = datetime.datetime(t1.year, t1.month, t1.day, t1.hour, t1.minute+1, t1.second, t1.microsecond)
-t3 = t2-t1
-
-while t3 !=0 and 0<=t3.days:
-        t1 = datetime.datetime.now()
-        t3 = t2-t1
-
-count = 1
-while count<=215:
-    path = 'frames/good.gif-' + str(count) + '.gif'
-    setWallpaper(path)
-    time.sleep(2)
-    count+=1
+x = GenFrames('frames', 'good.gif')
+x.extractFrames()
+x.getTimes()
